8aca3cd92d72109e1df81c3d3500788f14783a35
TheBillusBillus <thebillusbillus@gmail.com>
2017-11-22 19:26:51 -0500

    initial

---
+++ cpuLogger/src/cpuLogger/cpuLMain.java
@@ -0,0 +1,214 @@
+package cpuLogger;
+
+import java.util.Scanner;
+
+public class cpuLMain {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		/**
+		 * i is the main counter that defines the next empty position that can be filled
+		 * to prevent existing entries from being over written
+		 */
+		int i = 0;
+		int maxSize = 10;
+		cpuL[] store = new cpuL[maxSize];
+		
+
+		while (true) {
+			int opt = 10;
+			while (opt != 0 && opt != 1 && opt != 2 && opt != 3 && opt != 4 && opt != 5) {
+				System.out.println("CPU OVERCLOCKING LOG SYSTEM");
+				System.out.println("1. INPUT NEW LOG");
+				System.out.println("2. SEARCH EXISTING ENTRIES");
+				System.out.println("3. PRINT AN ENTRY");
+				System.out.println("4. READ FROM FILE");
+				System.out.println("5. WRITE TO FILE");
+				System.out.println("0. EXIT PROGRAM");
+				opt = sc.nextInt();
+			}
+			if (opt == 1) {
+				i = takeInput(i, maxSize, store);
+			} else if (opt == 2) {
+				search(store, maxSize);
+			} else if (opt == 3) {
+				print(store);
+			} else if (opt == 4) {
+				read();
+			} else if (opt == 5) {
+				write(store);
+			} else if (opt == 0) {
+				System.out.println("END OF PROGRAM");
+				System.exit(0);
+			} else {
+				System.out.println("INVALID INPUT");
+			}
+		}
+	}
+
+	public static int takeInput(int i, int maxSize, cpuL[] store) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("INPUT MODE");
+
+		String[] type = new String[maxSize];
+		String[] family = new String[maxSize];
+		String[] model = new String[maxSize];
+		String[] socket = new String[maxSize];
+		int[] freq = new int[maxSize];
+		int[] freqOC = new int[maxSize];
+		int[] fsb = new int[maxSize];
+		int[] width = new int[maxSize];
+		int[] core = new int[maxSize];
+		int[] thread = new int[maxSize];
+		float[] multiMin = new float[maxSize];
+		float[] multiMax = new float[maxSize];
+		float[] vcoreBase = new float[maxSize];
+		float[] vcoreMax = new float[maxSize];
+		float[] vcoreOC = new float[maxSize];
+
+		boolean read = true;
+
+		while (read == true) {
+			System.out.print("Type: ");
+			type[i] = sc.next().toUpperCase();
+			System.out.print("Family: ");
+			family[i] = sc.next().toUpperCase();
+			System.out.print("Model: ");
+			model[i] = sc.next().toUpperCase();
+			System.out.print("Socket: ");
+			socket[i] = sc.next().toUpperCase();
+			System.out.print("Base Clock Speed (MHz): ");
+			freq[i] = sc.nextInt();
+			System.out.print("OC Clock Speed (MHz): ");
+			freqOC[i] = sc.nextInt();
+			System.out.print("Base FSB Speed (MHz): ");
+			fsb[i] = sc.nextInt();
+			System.out.print("Data Width (bits): ");
+			width[i] = sc.nextInt();
+			System.out.print("Core Count: ");
+			core[i] = sc.nextInt();
+			System.out.print("Thread Count: ");
+			thread[i] = sc.nextInt();
+			System.out.print("Minimum Multiplier: ");
+			multiMin[i] = sc.nextFloat();
+			System.out.print("Maximum Multiplier: ");
+			multiMax[i] = sc.nextFloat();
+			System.out.print("Base Vcore (V): ");
+			vcoreBase[i] = sc.nextFloat();
+			System.out.print("Maximum Rated Vcore (V): ");
+			vcoreMax[i] = sc.nextFloat();
+			System.out.print("OC Vcore (V): ");
+			vcoreOC[i] = sc.nextFloat();
+
+			// end of taking inputs
+			i += 1; // shifts counter position for new data set
+			String contTest = "a";
+			while (!"y".equals(contTest) && !"n".equals(contTest)) {
+				System.out.println("Would you like to input another data set? (Y/N)");
+				contTest = sc.next().toLowerCase();
+			}
+			if (contTest.equals("n")) {
+				read = false;
+				System.out.println("END OF TAKING INPUTS");
+			}
+		}
+
+		for (int j = 0; j < type.length; j++) {
+			store[j] = new cpuL(type[j], family[j], model[j], socket[j], freq[j], freqOC[j], fsb[j], width[j], core[j],
+					thread[j], multiMin[j], multiMax[j], vcoreBase[j], vcoreMax[j], vcoreOC[j]);
+		}
+		return i;
+	}
+
+	public static void search(cpuL[] store, int maxSize) {
+		Scanner sc = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		String result = "|";
+		System.out.println("SEARCH MODE");
+		System.out.println("1. SEARCH BY FAMILY & MODEL");
+		System.out.println("2. SEARCH BY BASE CLOCK SPEED");
+		System.out.println("3. SEARCH BY OC CLOCK SPEED");
+		System.out.println("4. SEARCH FOR MAX OC SPEED");
+
+		int opt = sc.nextInt();
+
+		if (opt == 1) {
+			String family = null;
+			String model = null;
+			System.out.print("FAMILY: ");
+			family = sc2.nextLine().toUpperCase();
+			System.out.print("MODEL: ");
+			model = sc2.nextLine().toUpperCase();
+			for (int i = 0; i < maxSize; i++) {
+				if (store[i].family == null) {
+					break;
+				} else if (store[i].family.equals(family) && store[i].model.equals(model)) {
+					result = result + i + "|";
+				}
+				System.out.println(result);
+			}
+		} else if (opt == 2) {
+			System.out.print("Base Clock Speed (MHz): ");
+			int freq = sc.nextInt();
+			for (int i = 0; i < maxSize; i++) {
+				if (store[i].family == null) {
+					System.out.print("break");
+					break;
+				} else if (store[i].freq == freq) {
+					result = result + i + "|";
+				}
+			}
+			System.out.println(result);
+		} else if (opt == 3) {
+			System.out.print("OC Clock Speed (MHz): ");
+			int freqOC = sc.nextInt();
+			for (int i = 0; i < maxSize; i++) {
+				if (store[i].family == null) {
+					System.out.print("break");
+					break;
+				} else if (store[i].freqOC == freqOC) {
+					System.out.println(result);
+				}
+			}
+			result = result + "|";
+		} else if (opt == 4) {
+			int freqOC = 0;
+			for (int i = 0; i < maxSize; i++) {
+				if (store[i].family == null) {
+					System.out.print("break");
+					break;
+				}
+				if (store[i].freqOC > freqOC) {
+					freqOC = store[i].freqOC;
+				}
+			}
+			System.out.println(freqOC);
+		}
+	}
+
+	public static void print(cpuL[] store) {
+		System.out.println("PRINT MODE");
+		Scanner sc = new Scanner(System.in);
+		int in = 0;
+		System.out.print("LOG ID: ");
+		in = sc.nextInt();
+		System.out.println(store[in]);
+	}
+
+	public static void read() {
+		System.out.println("READ MODE");
+		/**
+		 * use read and split function
+		 */
+		// System.out.println("DATA HAS BEEN LOADED");
+	}
+
+	public static void write(cpuL[] store) {
+		System.out.println("WRITE MODE");
+		/**
+		 * write everything to file on one line
+		 */
+		// System.out.println("DATA HAS BEEN WRITTEN TO FILE");
+	}
+}
